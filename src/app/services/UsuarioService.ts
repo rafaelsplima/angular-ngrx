acfabfc60e93287cce0de7047c42407db28adad2
Rafael Lima <rafaelsplima@gmail.com>
2024-09-21 10:41:57 -0300

    feat: listagem usuario

--- src/app/services/UsuarioService.ts
+++ src/app/services/UsuarioService.ts
@@ -1,38 +1,37 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { UsuarioModel } from '../models/UsuarioModel';
-import { Inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 
-@Inject({
-  provideIn: 'root'
+@Injectable({
+   providedIn: 'root'
 })
 export class UsuarioService {
 
   private readonly URL = 'http://localhost:3000/usuarios';
+  headers = new HttpHeaders();
 
-
-  constructor(private htpp: HttpClient, private headers:HttpHeaders) {
-    headers = new HttpHeaders();
-    headers.set('Content-Type', 'application/json; charset=utf-8');
+  constructor(private htpp: HttpClient) {
+    this.headers.set('Content-Type', 'application/json; charset=utf-8');
   }
 
   getUsuarios(){
-    return this.htpp.get(this.URL);
+    return this.htpp.get<UsuarioModel[]>(this.URL);
   }
 
   getUsuario(id: number){
-    return this.htpp.get(`this.URL${id}`);
+    return this.htpp.get<UsuarioModel>(`this.URL${id}`);
   }
 
   addUsuario(usuario: UsuarioModel){
-    return this.htpp.post(this.URL,JSON.stringify(usuario), {headers: this.headers});
+    return this.htpp.post<UsuarioModel>(this.URL,JSON.stringify(usuario), {headers: this.headers});
   }
 
   updateUsuario(usuario: UsuarioModel){
-    return this.htpp.post(`this.URL${usuario.id}`,JSON.stringify(usuario), {headers: this.headers});
+    return this.htpp.post<UsuarioModel>(`this.URL${usuario.id}`,JSON.stringify(usuario), {headers: this.headers});
   }
 
   deleteUsuario(id: number){
-    return this.htpp.delete(`this.URL${id}`, {headers: this.headers})
+    return this.htpp.delete<UsuarioModel>(`this.URL${id}`, {headers: this.headers})
   }
 
 }
